5f5b5b70a796852225301cbe2a4caf6ebe79e831
Aryan <chaudharyaryan3456@gmail.com>
2025-06-24 23:26:35 +0530

    Revision Completed

---
+++ init.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Chat = require("./models/chat.js");
+
+main()
+    .then((res) => {
+        console.log("connection is successful");
+    })
+    .catch(err =>
+        console.log(err)
+    );
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+}
+
+
+let allchats = [
+    {
+        from: "Aryan",
+        to: "God",
+        msg: "Give me strength",
+        created_at: new Date()
+    },
+    {
+        from: "Aryan",
+        to: "Barfi",
+        msg: "Bhaag le thoda",
+        created_at: new Date()
+    },
+    {
+        from: "Aryan",
+        to: "Mammy",
+        msg: "Khana bna do",
+        created_at: new Date()
+    },
+    {
+        from: "Aryan",
+        to: "itself",
+        msg: "m lajwaab hu",
+        created_at: new Date()
+    },
+];
+Chat.insertMany(allchats); 
